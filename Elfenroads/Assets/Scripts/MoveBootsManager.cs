16137909c1016b942030f8af2c59cfa9b8a27d22
Xiang Meng <xiang.meng2@mail.mcgill.ca>
2021-10-22 22:26:10 -0400

    refined prefabs & set boot to spawn at start city

--- Elfenroads/Assets/Scripts/MoveBootsManager.cs
+++ Elfenroads/Assets/Scripts/MoveBootsManager.cs
@@ -5,26 +5,39 @@ using UnityEngine;
 
 public class MoveBootsManager : MonoBehaviour
 {
-
-
+    public GameObject Boot;
+    private Transform BootTransform;
 
     // Start is called before the first frame update
     void Start()
     {
+        setStaringPosition();
+
         //This line adds this script as a kind of "observer" to any changes in game state in the GameManager.
         GameManager.onBroadStateChange += GameManagerOnGameStateChanged;
     }
 
     //This function does something depending on the aforementioned game state change.
-    void GameManagerOnGameStateChanged(BroadGameState state){
-        if(state == BroadGameState.MoveBoot){
+    void GameManagerOnGameStateChanged(BroadGameState state)
+    {
+        if (state == BroadGameState.MoveBoot)
+        {
             Debug.Log("Time for MoveBoots!");
         }
+    }
+
+    // spawn the boot at ElfenHold:
+    void setStaringPosition()
+    {
+        GameObject startingCity = GameObject.Find("Elfenhold");
+        Boot.transform.position = startingCity.transform.position;
+        Debug.Log("Entered");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
