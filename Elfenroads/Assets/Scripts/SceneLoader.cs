951a87c2635fa67eb276136c215c3845d4969968
danial23 <5867710+danial23@users.noreply.github.com>
2021-10-24 21:13:52 -0400

    Connect MenuScene with MainScene
    Added SceneLoader.cs

---
+++ Elfenroads/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private Button button;
+
+	void Start () {
+		button = this.gameObject.GetComponent<Button>();
+		button.onClick.AddListener(LoadNextScene);
+	}
+
+	void LoadNextScene(){
+		Debug.Log ("Loading next scene");
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+	}
+}
