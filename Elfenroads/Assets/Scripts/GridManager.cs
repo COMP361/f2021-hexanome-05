e6b907a78b84fb0b8fbe0961a96b708ae4f5d95b
Xiang Meng <xiang.meng2@mail.mcgill.ca>
2021-10-26 15:32:30 -0400

    Added ? button (draggable)

---
+++ Elfenroads/Assets/Scripts/GridManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    private bool dragging;
+    public GameObject Grid;
+    public Vector3 originalPosition;
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(position);
+        }
+    }
+
+    void Start()
+    {
+        Grid.transform.position = originalPosition;
+    }
+
+    public void OnMouseDown()
+    {
+        dragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+
+}
