ea45b22c43c351800ec8514a512992bca09d4e89
Max <maxboucher45@gmail.com>
2022-01-17 17:24:44 -0500

    SampleScene testing
    - Thought of and tested a fairly basic "slot" system which would allow dragging/dropping of elements from UI onto GameObjects, as well as easy and dynamic rendering from update() events. (I'll make a more sophisticated version in TownView Scripts)
    - See sample scene for a short demo!

---
+++ Elfenroads/Assets/Scripts/SlotAssignTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotAssignTest : MonoBehaviour
+{
+    public GameObject boot;
+    public GameObject town;
+    public Button thisButton;
+
+    void Start()
+    {
+        thisButton.onClick.AddListener(addBoot);
+    }
+    
+    public void addBoot(){
+        town.GetComponent<SlotTester>().addToSlot(boot);
+    }
+
+}
