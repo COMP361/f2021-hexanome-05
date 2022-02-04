27da22b245e3e2cdf52f1355366f69a7ae4f2f00
danial23 <5867710+danial23@users.noreply.github.com>
2022-02-04 16:10:13 -0500

    Fix typo

--- Elfenroads/Assets/Scripts/Model/Cards/Card.cs
+++ Elfenroads/Assets/Scripts/Model/Cards/Card.cs
@@ -5,17 +5,17 @@ namespace Models
 {
     public abstract class Card
     {
-        public Guid id { set; get; }
+        public Guid id { protected set; get; }
         public int round; //Cards have rounds. It may be worth setting some kind of sorting order (items with lower rounds show up first in the CardPile list).
 
-        public bool Equals(Counter other) {
+        public bool Equals(Card other) {
             return other != null &&
                     other.id == this.id;
         }
 
         public override bool Equals(object obj)
         {   
-            return Equals(obj as Counter);
+            return Equals(obj as Card);
         }
 
         public override int GetHashCode()
