2affe24d0b45131e61a2810a2e117f25470195a2
xm2j <90661630+xm2j@users.noreply.github.com>
2022-03-07 19:55:39 -0500

    added dynamically changing help info

--- Elfenroads/Assets/Scripts/Control/AlwaysActiveUIController.cs
+++ Elfenroads/Assets/Scripts/Control/AlwaysActiveUIController.cs
@@ -8,11 +8,61 @@ public class AlwaysActiveUIController : MonoBehaviour
 {
 
     public GameObject helpWindow;
+    public TMPro.TMP_Text PhaseName;
+    public TMPro.TMP_Text PhaseExplanation;
+
 
 	void Start()
 	{
         helpWindow.SetActive(false);
+
+        // switch to display chooseBoot help info
+        //////////////////////////////////////////// To be edited later ////////////////////////
+        PhaseName.text = "This is choose boot";
+        PhaseExplanation.text = "A very long string, explanation for choose boot aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        //////////////////////////////////////////// To be edited later ////////////////////////
 	}
+
+    void Update(){
+        if (Elfenroads.Model.game.currentPhase is DrawCounters){
+            //////////////////////////////////////////// To be edited later ////////////////////////
+            PhaseName.text = "This is draw counters phase";
+            PhaseExplanation.text = "A very long string, explanation for draw counters aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            //////////////////////////////////////////// To be edited later ////////////////////////
+
+        }
+        else if (Elfenroads.Model.game.currentPhase is Auction){
+            //////////////////////////////////////////// To be edited later ////////////////////////
+            PhaseName.text = "This is auction phase";
+            PhaseExplanation.text = "A very long string, explanation for auction aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            //////////////////////////////////////////// To be edited later ////////////////////////
+        }
+        else if (Elfenroads.Model.game.currentPhase is FinishRound){
+            //////////////////////////////////////////// To be edited later ////////////////////////
+            PhaseName.text = "This is finish round phase";
+            PhaseExplanation.text = "A very long string, explanation for finish round aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            //////////////////////////////////////////// To be edited later ////////////////////////
+        }
+        else if (Elfenroads.Model.game.currentPhase is PlanTravelRoutes){
+            //////////////////////////////////////////// To be edited later ////////////////////////
+            PhaseName.text = "This is plan travel route phase";
+            PhaseExplanation.text = "A very long string, explanation for plan travel route aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            //////////////////////////////////////////// To be edited later ////////////////////////
+        }
+        else if (Elfenroads.Model.game.currentPhase is MoveBoot){
+            //////////////////////////////////////////// To be edited later ////////////////////////
+            PhaseName.text = "This is move boot phase";
+            PhaseExplanation.text = "A very long string, explanation for move boot aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            //////////////////////////////////////////// To be edited later ////////////////////////
+        }
+        ////////////////////////// more phases to be added once they are defined //////////////////
+        else {
+            //////////////////////////////////////////// To be edited later ////////////////////////
+            PhaseName.text = "This is choose boot";
+            PhaseExplanation.text = "A very long string, explanation for choose boot aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            //////////////////////////////////////////// To be edited later ////////////////////////
+        }
+    }
 
     public void CloseHelpWindow(){
         if(helpWindow.activeSelf){
