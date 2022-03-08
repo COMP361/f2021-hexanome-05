f9b6b4791f6f3cb4d3fb497c4c75760105762662
Max <maxboucher45@gmail.com>
2022-03-08 12:55:59 -0500

    Tweaked phase descriptions

--- Elfenroads/Assets/Scripts/Control/AlwaysActiveUIController.cs
+++ Elfenroads/Assets/Scripts/Control/AlwaysActiveUIController.cs
@@ -11,6 +11,12 @@ public class AlwaysActiveUIController : MonoBehaviour
     public TMPro.TMP_Text PhaseName;
     public TMPro.TMP_Text PhaseExplanation;
 
+    [HideInInspector]
+    public bool isOpen = false;
+    
+    private bool cameraOnOpen;
+    private bool draggablesOnOpen;
+
 
 	void Start()
 	{
@@ -23,27 +29,29 @@ public class AlwaysActiveUIController : MonoBehaviour
 
     public void UpdateDrawCounterHelp(){
         PhaseName.text = "Draw Counters";
-        PhaseExplanation.text = "Please click on the desired counter to keep it. You could also draw from the face-down counter pile." + "\n" +
-                                "Your inventory is shown at the bottom of the screen." + "\n" +
-                                "To view the ElfenRoads game map before selecting a counter, click the map button on the top right corner." + "\n" +
-                                "Once in the map view, you can press wasd to pan around the map, or zoom around with the mouse wheel." + "\n" +
-                                "To bring back the counter selection window, click on the map button again.";
+        PhaseExplanation.text = "The goal of this phase is to add counters to your inventory for use in the PlanTravel phase." + "\n" +
+                                "- Please click on the desired counter to keep it. You could also click the counter pile to draw randomly." + "\n" +
+                                "- Your inventory is shown at the bottom of the screen. Click 'expand' to see your cards!" + "\n" +
+                                "- The map button in the top right corner will toggle the map on and off." + "\n" +
+                                "- Once in the map view, you can use WASD to pan around the map, or zoom in/out with the mouse wheel.";
     }
 
     public void UpdatePlanTravelRoutesHelp(){
-        PhaseName.text = "Plan Travel Route";
-        PhaseExplanation.text = "You can press wasd to pan around the map, or zoom around with the mouse wheel." + "\n" + 
-                                "To place a counter/obstacle on the road, drag the item from your inventory to the road." + "\n" +
-                                "If you do not have anymore counters to place, click pass.";
+        PhaseName.text = "Plan Travel Routes";
+        PhaseExplanation.text = "The goal of this phase is to place counters, enabling you to use cards on them in the next phase." + "\n" + 
+                                "- You can use WASD to pan around the map, or zoom in/out with the mouse wheel." + "\n" + 
+                                "- To place a counter/obstacle on the road, drag the item from your inventory to the road." + "\n" +
+                                "- If you do not have anymore counters to place, click pass.";
     }
 
     
     public void UpdateMoveBootHelp(){
         PhaseName.text = "Move Your ElfenBoot";
-        PhaseExplanation.text = "You can press wasd to pan around the map, or zoom with the mouse wheel." + "\n" +
-                                "To move your boot, you can either click on the city that you wish to travel to, or the road/river that you wish to take." + "\n" +
-                                "You can take many moves untill you are satisfied." + "\n" +
-                                "After that, clicking on the EndTurn button will take your boot to the selected destination and bring you to select travel cards that you wish to keep.";
+        PhaseExplanation.text = "The goal of this phase is to play cards on roads with counters to visit Towns and earn points!" + "\n" +
+                                "You can use WASD to pan around the map, or zoom in/out with the mouse wheel." + "\n" +
+                                "To move your boot, drag the card type you want to use on the target road, lake or stream." + "\n" +
+                                "You can take many moves until you are satisfied, at which point you can click 'EndTurn'." + "\n" +
+                                "However, if you have more than 4 cards when you try to end, you must discard any extras.";
     }
 
     
